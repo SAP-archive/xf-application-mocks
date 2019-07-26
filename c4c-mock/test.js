175cfa79922bf5bb042b99ccf099a8661d2b7955
Thaler, Andreas <andreas.thaler01@sap.com>
2019-07-26 15:50:42 +0200

    updated dependencies

--- c4c-mock/test.js
+++ c4c-mock/test.js
@@ -9,19 +9,19 @@ describe('tests odata controllers', function () {
         this.timeout(10000);
         mock.then(function (app) {
 
-            describe('GET ServiceRequestInteraction via API', function () {
+            describe('GET AnswerAttachments via API', function () {
                 it('should return 200', function (done) {
                     request(app)
-                        .get('/sap/byd/api/v1/c4codata/ServiceRequestInteractions')
+                        .get('/sap/c4c/api/v1/c4codataapi/AnswerAttachments')
                         .set('Accept', 'application/json')
                         .expect('Content-Type', 'application/json; charset=utf-8')
                         .expect(200, done)
                 });
             });
-            describe('GET ServiceRequestInteraction via ODATA', function () {
+            describe('GET AnswerAttachments via ODATA', function () {
                 it('should return 200', function (done) {
                     request(app)
-                        .get('/sap/byd/odata/v1/c4codata/ServiceRequestInteractions')
+                        .get('/sap/c4c/odata/v1/c4codataapi/AnswerAttachments')
                         .set('Accept', 'application/json')
                         .expect('Content-Type', 'application/json; charset=utf-8')
                         .expect(200, done)
@@ -30,7 +30,7 @@ describe('tests odata controllers', function () {
             describe('GET odata metadata', function () {
                 it('should return 200', function (done) {
                     request(app)
-                        .get('/sap/byd/odata/v1/c4codata/$metadata')
+                        .get('/sap/c4c/odata/v1/c4codataapi/$metadata')
                         .expect('Content-Type', 'application/xml; charset=utf-8')
                         .expect(200, done)
                 });
@@ -38,7 +38,7 @@ describe('tests odata controllers', function () {
             describe('GET console', function () {
                 it('should return 200', function (done) {
                     request(app)
-                        .get('/sap/byd/api/v1/c4codata/console/')
+                        .get('/sap/c4c/api/v1/c4codataapi/console/')
                         .set('Accept', 'application/json')
                         .expect('Content-Type', 'text/html; charset=UTF-8')
                         .expect(200, done)
